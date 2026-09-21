027975291f68d4133167647f6f4605670b6c7697
aant <wichittar.kong@gmail.com>
2026-09-21 21:40:38 +0700

    fix: tolerate optional lifecycle controls

--- forms/substitute-receipt.logic.browser.js
+++ forms/substitute-receipt.logic.browser.js
@@ -142,7 +142,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function setMutationControlsDisabled(disabled) {
     [saveDraftButton, submitForApprovalButton, approveReceiptButton, receiveStockButton, completeReceiptButton, confirmReceiveBeforeCompleteButton, form.querySelector("button[type=reset]")]
-      .forEach((button) => { button.disabled = disabled; });
+      .forEach((button) => { if (button) button.disabled = disabled; });
   }
 
   function setLegacyReadOnly(readOnly) {
