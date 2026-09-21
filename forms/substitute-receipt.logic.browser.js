3ede786e445430722141237700bd86328a463695
aant <wichittar.kong@gmail.com>
2026-09-21 21:42:40 +0700

    fix: restore SR modal decline control

--- forms/substitute-receipt.logic.browser.js
+++ forms/substitute-receipt.logic.browser.js
@@ -141,7 +141,7 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   function setMutationControlsDisabled(disabled) {
-    [saveDraftButton, submitForApprovalButton, approveReceiptButton, receiveStockButton, completeReceiptButton, confirmReceiveBeforeCompleteButton, form.querySelector("button[type=reset]")]
+    [saveDraftButton, submitForApprovalButton, approveReceiptButton, receiveStockButton, completeReceiptButton, confirmReceiveBeforeCompleteButton, declineReceiveBeforeCompleteButton, form.querySelector("button[type=reset]")]
       .forEach((button) => { if (button) button.disabled = disabled; });
   }
 
